46b0d4af5ede8370baad037fa479bd17e0633fb8
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-16 13:26:39 +0300

    login screen ui update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField/TextField'
-import Typography from '@material-ui/core/Typography/Typography'
+import TextField from '@material-ui/core/TextField/TextField';
+import Typography from '@material-ui/core/Typography/Typography';
+import LoginStyle from './Login.css';
+import Card from '@material-ui/core/Card/Card';
+import { CardContent } from '@material-ui/core';
 
 class Login extends React.Component {
     render() {
         return(
-        <div>
-            <Typography variant="headline" color="primary">
-            gidjgijdigjiogjiodj
-            </Typography>
-            <TextField 
-                autoFocus={true}
-                name="login-email"
-                placeholder="Email ID"
-                type="email"
-                required="true"
-                fullWidth="true"
-            />
-            <TextField 
-                autoFocus={true}
-                name="login-password"
-                placeholder="Password"
-                type="password"
-                required="true"
-                fullWidth="true"
-            />
+        <div className="container">
+            <Card className='card-container'>
+                <CardContent>
+                    <Typography variant="headline" color="primary">
+                    LOGIN
+                    </Typography>
+                    <TextField 
+                        autoFocus={true}
+                        name="login-email"
+                        placeholder="Email ID"
+                        type="email"
+                        required="true"
+                        fullWidth="true"
+                    />
+                    <TextField 
+                        autoFocus={true}
+                        name="login-password"
+                        placeholder="Password"
+                        type="password"
+                        required="true"
+                        fullWidth="true"
+                    />
+                </CardContent>
+            </Card>
         </div>
         );
     }
