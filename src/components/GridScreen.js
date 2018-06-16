f82efd21ff3b3cd855d216fdb49700a4ef441d9a
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-16 20:14:56 +0300

    add test data

--- src/components/GridScreen.js
+++ src/components/GridScreen.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import GridStyle from './GridScreen.css';
 
 class GridScreen extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        subjects: ['DWM', 'OS', 'Android', 'iOS', 'DS', 'EM', 'MPMC', 'WTF', 'HOI', 'HELLO', 'OSS']
+    };
+
     render() {
         return(
-            <div></div>
+            <ul>
+                {this.state.subjects.map(subject => (
+                    <li>{subject}</li>))}
+            </ul>
         );
     }
 }
