3098c0af95c02a9a4ad71d1a0e1c789dc4b61170
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-18 15:49:09 +0300

    upload screen base layout done

--- src/components/UploadScreen.js
+++ src/components/UploadScreen.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, LinearProgress } from '@material-ui/core';
 import FileUpload from '@material-ui/icons/FileUpload'
+import UploadScreenStyles from './UploadScreen.css'
 
 class UploadScreen extends Component {
 
     render() {
         return (
-            <Button variant="contained" color="default">
-                Upload
-                <FileUpload />
-            </Button>
+            <div className="root-container">
+                <LinearProgress className="linear-progress" color="primary" variant="determinate" value={64} />
+                <Button variant="contained" color="default">
+                    Upload
+                    <FileUpload />
+                </Button>
+            </div>
         );
     };
 };
