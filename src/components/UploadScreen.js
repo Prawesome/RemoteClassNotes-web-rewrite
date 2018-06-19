978918a09b7d5ac070117d86653af65e0bdb27e8
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-19 08:52:18 +0300

    fixed file input

--- src/components/UploadScreen.js
+++ src/components/UploadScreen.js
@@ -8,11 +8,15 @@ class UploadScreen extends Component {
     render() {
         return (
             <div className="root-container">
+            //TODO: MULTIPLE FILE SELECTED MANAGER
                 <LinearProgress className="linear-progress" color="primary" variant="determinate" value={64} />
-                <Button variant="contained" color="default">
+                <input id="input-file-selector" name="input-file-selector" type="file" className="hidden-input" onChange={(event) => console.log(event.target.files[0])} />
+                <label htmlFor="input-file-selector" >
+                    <Button variant="contained" color="default" component="span" disableRipple={true} >
                     Upload
-                    <FileUpload />
-                </Button>
+                        <FileUpload />
+                    </Button>
+                </label>
             </div>
         );
     };
