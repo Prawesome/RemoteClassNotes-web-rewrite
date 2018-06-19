e815dd038e5f8f84465329c074cfb953115befb7
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-19 17:10:28 +0300

    conditional static rendering

--- src/components/GridScreen.js
+++ src/components/GridScreen.js
@@ -9,60 +9,64 @@ class GridScreen extends Component {
     super(props);
 
     this.state = {
-        subjectSelected: false
-    }
+      subjectSelected: false
+    };
+
+    this.files = [
+      {
+        name: "Hi.ppt",
+        subject: "DWM"
+      },
+      {
+        name: "Hello",
+        subject: "Andrid"
+      },
+      {
+        name: "MS.doc",
+        subject: "OS"
+      },
+      {
+        name: "Poda.patti",
+        subject: "odra"
+      }
+    ];
+    this.subjects = [
+      "DWM",
+      "OS",
+      "Android",
+      "iOS",
+      "DS",
+      "EM",
+      "MPMC",
+      "WTF",
+      "HOI",
+      "HELLO",
+      "OSS"
+    ];
   }
-
-  files = [
-    {
-      name: "Hi.ppt",
-      subject: "DWM"
-    },
-    {
-      name: "Hello",
-      subject: "Andrid"
-    },
-    {
-      name: "MS.doc",
-      subject: "OS"
-    },
-    {
-      name: "Poda.patti",
-      subject: "odra"
-    }
-  ];
-
-  subjects = [
-    "DWM",
-    "OS",
-    "Android",
-    "iOS",
-    "DS",
-    "EM",
-    "MPMC",
-    "WTF",
-    "HOI",
-    "HELLO",
-    "OSS"
-  ];
 
   //TODO: ROUTING
   //TODO: SHIT TON OF REFACTORNING TO BE DONE
 
   render() {
+    let cards;
+    if (this.state.subjectSelected) {
+      cards = this.files.map((file, index) => (
+        <GridListTile key={index} className="grid-item-container">
+          <DownloadCard file={file} />
+        </GridListTile>
+      ));
+    } else {
+      cards = this.subjects.map((subject, index) => (
+        <GridListTile key={index} className="grid-item-container">
+          <SubjectCard subject={subject} />
+        </GridListTile>
+      ));
+    }
     return (
       <div className="grid-outter-container">
         <GridList cols={5} className="grid-inner-container">
-          {this.files.map((file, index) => (
-            <GridListTile key={index} className="grid-item-container">
-              <DownloadCard file={file} />
-            </GridListTile>
-          ))};
-          {this.subjects.map((subject, index) => (
-            <GridListTile key={index} className="grid-item-container">
-              <SubjectCard subject={subject} />
-            </GridListTile>
-          ))}
+          {cards}
         </GridList>
       </div>
     );
