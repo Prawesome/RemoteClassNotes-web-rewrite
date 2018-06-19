5caa7e7d77f115835df2fcba24122e3e1c46a818
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-19 16:34:46 +0300

    temporarily use a single component for download and subjects

--- src/components/GridScreen.js
+++ src/components/GridScreen.js
@@ -1,75 +1,102 @@
-import React, { Component } from 'react';
-import GridStyle from './GridScreen.css';
-import { GridList} from '@material-ui/core';
-import DownloadGrid from './DownloadGrid';
-import { GridListTile, Card, CardContent, Typography, Divider } from '@material-ui/core'
+import React, { Component } from "react";
+import GridStyle from "./GridScreen.css";
+import { GridList } from "@material-ui/core";
+import DownloadGrid from "./DownloadGrid";
+import {
+  GridListTile,
+  Card,
+  CardContent,
+  Typography,
+  Divider
+} from "@material-ui/core";
 
 class GridScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
+  files = [
+    {
+      name: "Hi.ppt",
+      subject: "DWM"
+    },
+    {
+      name: "Hello",
+      subject: "Andrid"
+    },
+    {
+      name: "MS.doc",
+      subject: "OS"
+    },
+    {
+      name: "Poda.patti",
+      subject: "odra"
     }
+  ];
 
-    files = [
-        {
-            name: 'Hi.ppt',
-            subject: 'DWM'
-        }, 
-        {
-            name: 'Hello',
-            subject: 'Andrid'
-        },
-        {
-            name: 'MS.doc',
-            subject: 'OS'
-        },
-        {
-            name: 'Poda.patti',
-            subject: 'odra'
-        }
-    ];
+  subjects = [
+    "DWM",
+    "OS",
+    "Android",
+    "iOS",
+    "DS",
+    "EM",
+    "MPMC",
+    "WTF",
+    "HOI",
+    "HELLO",
+    "OSS"
+  ];
 
-    subjects = ['DWM', 'OS', 'Android', 'iOS', 'DS', 'EM', 'MPMC', 'WTF', 'HOI', 'HELLO', 'OSS'];
+  //TODO: ROUTING
+  //TODO: SHIT TON OF REFACTORNING TO BE DONE
+
+  render() {
+
+    let subjectCards = (
+
+    );
     
-    //TODO: ROUTING
-    //TODO: SHIT TON OF REFACTORNING TO BE DONE
-
-    render() {
-        return(
-            <div className="grid-outter-container">
-                <GridList cols={5} className="grid-inner-container">
-
-                {this.files.map( (file, index) => (
-                <GridListTile key={index} >
-                    <Card>
-                        <CardContent>
-                            <Typography variant="title" color="primary" className="center-text">
-                            {file.name}
-                            </Typography>
-                            <Divider />
-                            <Typography variant="body1" color="secondary">
-                            {file.subject}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </GridListTile>
-            ))};
-                    
-                    {/* {this.subjects.map( (subject, index) => 
-                    <GridListTile key={index} className="grid-item-container center-text">
-                        <Card className="card">
-                            <CardContent>
-                                <Typography variant="subheading" color="primary">
-                                    {subject}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </GridListTile>
-                    )} */}
-                </GridList>
-            </div>
-        );
-    }
+    return (
+      <div className="grid-outter-container">
+        <GridList cols={5} className="grid-inner-container">
+          {this.files.map((file, index) => (
+            <GridListTile key={index} className="grid-item-container">
+              <Card>
+                <CardContent>
+                  <Typography
+                    variant="title"
+                    color="primary"
+                    align="center"                    
+                  >
+                    {file.name}
+                  </Typography>
+                  <Divider />
+                  <Typography variant="body1" color="secondary">
+                    {file.subject}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </GridListTile>
+          ))};
+          {/* {this.subjects.map((subject, index) => (
+            <GridListTile
+              key={index}
+              className="grid-item-container"
+            >
+              <Card className="card">
+                <CardContent>
+                  <Typography variant="subheading" color="primary">
+                    {subject}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </GridListTile>
+          ))} */}
+        </GridList>
+      </div>
+    );
+  }
 }
 
 export default GridScreen;
--- src/components/UploadScreen.js
+++ src/components/UploadScreen.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
-import { Button, LinearProgress } from '@material-ui/core';
-import FileUpload from '@material-ui/icons/FileUpload'
-import UploadScreenStyles from './UploadScreen.css'
+import React, { Component } from "react";
+import { Button, LinearProgress } from "@material-ui/core";
+import FileUpload from "@material-ui/icons/FileUpload";
+import UploadScreenStyles from "./UploadScreen.css";
 
 class UploadScreen extends Component {
-//TODO: MULTIPLE FILE SELECTED MANAGER
-    render() {
-        return (
-            <div className="root-container">
-                <LinearProgress className="linear-progress" color="primary" variant="determinate" value={64} />
-                <input id="input-file-selector" name="input-file-selector" type="file" className="hidden-input" onChange={(event) => console.log(event.target.files[0])} />
-                <label htmlFor="input-file-selector" >
-                    <Button variant="contained" color="default" component="span" disableRipple={true} >
-                    Upload
-                        <FileUpload />
-                    </Button>
-                </label>
-            </div>
-        );
-    };
-};
+  //TODO: MULTIPLE FILE SELECTED MANAGER
+  render() {
+    return (
+      <div className="root-container">
+        <LinearProgress
+          className="linear-progress"
+          color="primary"
+          variant="determinate"
+          value={64}
+        />
+        <input
+          id="input-file-selector"
+          name="input-file-selector"
+          type="file"
+          className="hidden-input"
+          onChange={event => console.log(event.target.files[0])}
+        />
+        <label htmlFor="input-file-selector">
+          <Button
+            variant="contained"
+            color="default"
+            component="span"
+            disableRipple={true}
+          >
+            Upload <FileUpload />
+          </Button>
+        </label>
+      </div>
+    );
+  }
+}
 
 export default UploadScreen;
