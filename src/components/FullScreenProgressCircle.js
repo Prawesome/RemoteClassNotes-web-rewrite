fdd165e2e8bb9e827d36fd9971305c25ab267c2c
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-24 11:06:33 +0300

    Created progress component for full screen loads

---
+++ src/components/FullScreenProgressCircle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Grid.css'
+
+const FullScreenProgressCircle = () => {
+    return (
+        <CircularProgress size="40" thickness={4.0}  className="progress-circle"/>
+    )
+}
+
+export default FullScreenProgressCircle
