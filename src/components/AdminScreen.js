6ea1d9a3cc0b96e66d47c9f9c72548c467b7503f
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-04 06:39:02 +0530

    some edits to admin screen

--- src/components/AdminScreen.js
+++ src/components/AdminScreen.js
@@ -1,34 +1,45 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import firebase from "./Firebase";
+import FacultyEntry from "./FacultyEntry";
+import { Button, TextField } from "@material-ui/core";
 
 class AdminScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        faculties: []
+      faculties: [],
+      currentFaculty: ""
     };
 
     this.dbRef;
   }
 
   componentDidMount() {
-      this.getData();
-  };
+    this.getData();
+  }
 
   componentWillUnmount() {
-      this.dbRef().off();
-  };
+    this.dbRef().off();
+  }
 
   getData = () => {
-      this.dbRef = firebase.database().ref("/faculties");
+    this.dbRef = firebase.database().ref("/faculties");
 
-      this.dbRef.on('child_added', snapshot => {
-        this.setState({
-            faculties: [...this.state.faculties, snapshot.val()]
-        });
+    this.dbRef.on("child_added", snapshot => {
+      this.setState({
+        faculties: [...this.state.faculties, snapshot.val()]
       });
+    });
+  };
+
+  addFaculty = () => {};
+
+  facultyFieldHandler = (event) => {
+    this.setState({
+      currentFaculty: event.target.value
+    }) ;
   }
 
   render() {
@@ -42,7 +53,12 @@ class AdminScreen extends Component {
       <div>
         <NavBar title="Admin Dashboard" />
         <table>
-
+          {facultyEntries}
+          
+          <TextField value={this.state.currentFaculty} onChange={this.facultyFieldHandler} />
+          <Button variant="raised" color="primary" onClick={this.addFaculty}>
+            Add Faculty
+          </Button>
         </table>
       </div>
     );
