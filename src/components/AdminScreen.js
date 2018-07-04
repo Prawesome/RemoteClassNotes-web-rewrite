c03913916925cacf8dedd0c328a1105bc97c3e80
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-04 20:03:30 +0530

    fix table not rendering bug

--- src/components/AdminScreen.js
+++ src/components/AdminScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import NavBar from "./NavBar";
 import firebase from "./Firebase";
 import FacultyEntry from "./FacultyEntry";
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Table, TableHead, TableCell, TableBody } from "@material-ui/core";
 
 class AdminScreen extends Component {
   constructor(props) {
@@ -34,32 +34,42 @@ class AdminScreen extends Component {
     });
   };
 
-  addFaculty = () => {};
+  addFaculty = () => {
+    this.dbRef.push(this.state.currentFaculty);
+  };
 
-  facultyFieldHandler = (event) => {
+  facultyFieldHandler = event => {
     this.setState({
       currentFaculty: event.target.value
-    }) ;
-  }
+    });
+  };
 
   render() {
     let facultyEntries;
 
-    facultyEntries = this.state.faculties.map((faculty, index) => {
-      <FacultyEntry key={index} faculty={faculty} />;
-    });
+    facultyEntries = this.state.faculties.map((faculty, index) => (
+      <FacultyEntry key={index} faculty={faculty} />
+    ));
 
     return (
       <div>
         <NavBar title="Admin Dashboard" />
-        <table>
-          {facultyEntries}
-          
-          <TextField value={this.state.currentFaculty} onChange={this.facultyFieldHandler} />
+        <TextField
+            value={this.state.currentFaculty}
+            onChange={this.facultyFieldHandler}
+          />
           <Button variant="raised" color="primary" onClick={this.addFaculty}>
             Add Faculty
           </Button>
-        </table>
+        <Table>
+          <TableHead>
+            <TableCell>Faculty</TableCell>
+            <TableCell>Approve</TableCell>
+          </TableHead>
+          <TableBody>
+            {facultyEntries}
+          </TableBody>
+        </Table>
       </div>
     );
   }
